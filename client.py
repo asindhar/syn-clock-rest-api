e35f25af1c9dd8dd5cc2590e0066f0e37d056edb
aman <aman.s.s@outlook.com>
2019-02-07 10:29:56 -0800

    added RTT

--- client.py
+++ client.py
@@ -1,12 +1,34 @@
 import requests, subprocess
+from datetime import datetime
 
 def syncTime():
 	#Api url to fetch date string from server
 	api_url = 'http://localhost:5000/'
 
+	#Start timer for RTT
+	t1 = datetime.now()
+
+	#Get data from api endpoint
 	#accepted format of date is month:day:hour:min:year.sec
 	res = requests.get(api_url).json()
-	return res['dt']
+	
+	#Stop timer for RTT
+	t2 = datetime.now()
+	RTT = (t2 - t1) / 2
+	
+	print("Date-Time received from server: ", res['dt'])
+	print("RTT: ", RTT)
+
+	#Add RTT to server time
+	dt = datetime.strptime(res['dt'], '%Y-%m-%d %H:%M:%S.%f') + RTT
+	print("Date-Time with RTT: ", dt)
+
+	#format the date object to a string - month:day:hour:min:year.sec 
+	#ex 0206180219.59
+	# Unix date command only change time upto seconds level
+	dt_str = dt.strftime('%m%d%H%M%y.%S')
+	
+	return dt_str
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -6,14 +6,11 @@ app = Flask(__name__)
 api = Api(app)
 
 class serverTime(Resource):
-	"""docstring for class"""
 	def get(self):
 		#get server time
 		dt = datetime.datetime.now()
-		#format the date object to a string - month:day:hour:min:year.sec 
-		#ex 0206180219.59
-		dt_str = dt.strftime('%m%d%H%M%y.%S')
-		return {'dt': dt_str}, 200
+		
+		return {'dt': str(dt)}, 200
 		
 api.add_resource(serverTime, '/')
 
